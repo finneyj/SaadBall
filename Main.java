942c4d71999eeba11d5dd313c8d4b1cc9ba77b0f
Joe Finney <joe@comp.lancs.ac.uk>
2024-02-27 17:22:53 +0000

    Added star effect to Main

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ public class Main
     public static void main(String[] args)
     {
         GameArena arena = new GameArena(500,400);
-
         SmallSpaceShip s = new SmallSpaceShip(100, 100, arena, 10);
+        Starfield field = new Starfield(150, 5, arena);
 
         while(true)
         {
+            field.move();
+
             if (arena.rightPressed()) {
                 s.move(5, 0);
               }
